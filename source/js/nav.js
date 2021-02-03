3724d84d14a87002cb6bc1a0a31542209f8f0978
koskos1986 <ekosopletkin@yandex.ru>
2021-02-03 09:33:01 +0300

    Финальные правки

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,24 +1,24 @@
 (() => {
-  const pageClass = {
+  const navToggle = {
     NOJS: `header__container--nojs`,
     OPEN: `header__container--open`
   };
   const header = document.querySelector(`.js-header`);
   const menu = header.querySelector(`.js-menu`);
-  const openBtn = header.querySelector(`.js-button-open`);
-  const closeBtn = header.querySelector(`.js-button-close`);
+  const openButton = header.querySelector(`.js-button-open`);
+  const closeButton = header.querySelector(`.js-button-close`);
 
   const onHeaderToggleClick = (evt) => {
-    if (evt.target === openBtn) {
-      menu.classList.add(pageClass.OPEN);
+    if (evt.target === openButton) {
+      menu.classList.add(navToggle.OPEN);
     }
-    if (evt.target === closeBtn) {
-      menu.classList.remove(pageClass.OPEN);
+    if (evt.target === closeButton) {
+      menu.classList.remove(navToggle.OPEN);
     }
   };
 
-  menu.classList.remove(pageClass.NOJS);
-  openBtn.style.display = `block`;
+  menu.classList.remove(navToggle.NOJS);
+  openButton.style.display = `block`;
 
   header.addEventListener(`click`, onHeaderToggleClick);
 })();
